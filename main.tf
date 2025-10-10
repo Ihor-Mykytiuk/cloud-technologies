c38ab8565c644cf555a4d51ceed3d88b977d3e10
Ihor Mykytiuk <igor14vn@gmail.com>
2025-10-10 23:22:19 +0300

    added resource group policy

--- main.tf
+++ main.tf
@@ -4,3 +4,23 @@ resource "azurerm_resource_group" "rg" {
   tags     = var.tags
 }
 
+data "azurerm_policy_definition" "require_tag" {
+  display_name = "Require a tag and its value on resources"
+}
+
+resource "azurerm_resource_group_policy_assignment" "require_tag" {
+  name                  = "require-cost-center-tag"
+  description           = "Require Cost Center tag and its value on all resources in the resource group"
+  resource_group_id     = azurerm_resource_group.rg.id
+  policy_definition_id  = data.azurerm_policy_definition.require_tag.id
+  enforce               = true
+
+  parameters = jsonencode({
+    "tagName" = { 
+        value = keys(var.tags)[0] 
+    },
+    "tagValue" = {
+        value = values(var.tags)[0]
+    }
+})
+}
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "resource_group_name" {
   description = "The Name of the created resource group."
   value       = azurerm_resource_group.rg.name
 }
+
+output "require_policy_assignment_id" {
+  description = "The ID of the 'Require Tag' policy assignment."
+  value       = azurerm_resource_group_policy_assignment.require_tag.id
+}
