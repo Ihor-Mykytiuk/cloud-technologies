5a022562e6603f80171367a329c5dd9f2e42ddcf
Ihor Mykytiuk <igor14vn@gmail.com>
2025-10-11 00:21:16 +0300

    changed policy

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,17 @@ output "resource_group_name" {
   value       = azurerm_resource_group.rg.name
 }
 
-output "require_policy_assignment_id" {
-  description = "The ID of the 'Require Tag' policy assignment."
-  value       = azurerm_resource_group_policy_assignment.require_tag.id
+# output "require_policy_assignment_id" {
+#   description = "The ID of the 'Require Tag' policy assignment."
+#   value       = azurerm_resource_group_policy_assignment.require_tag.id
+# }
+
+output "inherit_policy_assignment_id" {
+  description = "The ID of the 'Inherit Tag' policy assignment."
+  value       = azurerm_resource_group_policy_assignment.inherit_tag.id
+}
+
+output "storage_account_name" {
+  description = "The name of the created test storage account."
+  value       = azurerm_storage_account.testsa.name
 }
