773dfd6cba746772822ad2941f8ac44b499e2e5e
Ihor Mykytiuk <igor14vn@gmail.com>
2025-10-11 15:14:56 +0300

    created storrage account

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "random_string" "sa_name" {
+  length  = 16
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "cloudshell_storage" {
+  name                     = "st${random_string.sa_name.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,7 @@ output "resource_group_name" {
   value       = azurerm_resource_group.rg.name
 }
 
+output "cloud_shell_storage_account_name" {
+  description = "Name of the storage account created for Cloud Shell."
+  value       = azurerm_storage_account.cloudshell_storage.name
+}
