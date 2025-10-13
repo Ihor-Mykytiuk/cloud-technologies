4edb0f11507a73828e369417d5302a79a0b1e7e4
Ihor Mykytiuk <igor14vn@gmail.com>
2025-10-14 01:34:22 +0300

    created and configured storrage account

--- main.tf
+++ main.tf
@@ -2,3 +2,48 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "random_string" "storage_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+data "http" "my_public_ip" {
+  url = "https://ipv4.icanhazip.com"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "az104-lab7${random_string.storage_suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+  account_kind             = "StorageV2"
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = [chomp(data.http.my_public_ip.response_body)]
+    bypass                     = ["AzureServices"]
+    virtual_network_subnet_ids = []
+  }
+}
+
+resource "azurerm_storage_management_policy" "lifecycle_policy" {
+  storage_account_id = azurerm_storage_account.storage.id
+
+  rule {
+    name    = "Movetocool"
+    enabled = true
+
+    filters {
+      blob_types = ["blockBlob"]
+    }
+
+    actions {
+      base_blob {
+        tier_to_cool_after_days_since_modification_greater_than = 30
+      }
+    }
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "resource_group_name" {
   description = "The name of the created resource group"
   value       = azurerm_resource_group.rg.name
 }
+
+output "storage_account_name" {
+  description = "The name of the created storage account"
+  value       = azurerm_storage_account.storage.name
+}
+
+output "your_detected_public_ip" {
+  description = "Your public IP address that has been added to the firewall rules"
+  value       = chomp(data.http.my_public_ip.response_body)
+}
